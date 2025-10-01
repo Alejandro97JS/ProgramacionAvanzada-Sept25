af4cc10f485928495e8094c607ee4f8a6bf9bb13
Alejandro JS <alejandro97js@gmail.com>
2025-10-01 23:08:06 +0200

    feat(interesting_stuff): Added more example and edge cases

--- Clase1/interesting_stuff.py
+++ Clase1/interesting_stuff.py
@@ -34,5 +34,23 @@ def main():
     c = test1()
     print(c)
 
+    # Aprovechar que Python admite ambos tipos de comillas:
+    d = {
+        "clave": "Hola"
+    }
+    my_str = f"¡¡{d.get("clave")}!!"
+    print(my_str)
+
+    my_list = [1,2,3]
+    my_list_por_dos = map(lambda x: 2*x, my_list)
+    print(list(my_list_por_dos))
+
+    # Clase hipoteca:
+    my_hipoteca = Mortgage(50000, 2000, 30)
+    my_hipoteca.imprimir_resultado()
+    my_hipoteca.increase_quantity(100000)
+    my_hipoteca._calculate()
+    my_hipoteca.imprimir_resultado()
+
 if __name__ == "__main__":
     main()
