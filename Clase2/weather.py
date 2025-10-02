2d1d355bedaa82969f99ea7a948e0c2d635ad52c
Antonio Rodriguez <antojrd@gmail.com>
2025-10-02 20:42:23 +0200

    Ultima version

--- Clase2/weather.py
+++ Clase2/weather.py
@@ -1,5 +1,5 @@
 import requests
-
+from pprint import pprint
 
 class WeatherAPI:
     base_url = "https://api.open-meteo.com/v1/forecast"
@@ -17,8 +17,15 @@ class WeatherAPI:
         else:
             self.data = None
 
-    def print_data(self):
-        location_name = self.data
+    def print_data(self, address):
+        print(f"Historial de Temperaturas para {address}")
+        print("------------------------------------")
+        data = weather.data
+        horas = data["hourly"]["time"]
+        temperaturas = data["hourly"]["temperature_2m"]
+        for hora, temp in zip(horas, temperaturas):
+            print(f"{hora} → {temp} °C")
+
 
 class LocationAPI:
     base_url = "https://nominatim.openstreetmap.org/search"
@@ -36,7 +43,7 @@ class LocationAPI:
         }
         response = requests.get(self.base_url, params=params, headers=headers)
         if response.status_code == 200:
-            print(f"response {response.text}")
+
             data = response.json()
             if data:
                 return {'longitude': data[0]["lon"], 'latitude': data[0]["lat"] }
@@ -49,10 +56,11 @@ class LocationAPI:
 
 
 if __name__ == "__main__":
-    latitude = -9.4154
-    longitude = -3.7074
+
+    address = "Cádiz, España"
     location = LocationAPI()
-    coordinates = location.get_coordinates("Cádiz, España")
+    coordinates = location.get_coordinates(address)
     weather = WeatherAPI(coordinates.get('latitude'), coordinates.get('longitude'))
     weather.request_data("&hourly=temperature_2m&current=temperature_2m,precipitation,rain,relative_humidity_2m,apparent_temperature,is_day")
-    print(weather.data)
+    #print(weather.data)
+    weather.print_data(address)
